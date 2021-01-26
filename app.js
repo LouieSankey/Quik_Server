e279a3b91a4596c44cbdccab7e694edf106b8107
louissankey <lmsankey@ucdavis.edu>
2021-01-25 18:06:19 -0800

    deploy

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Here's a vanilla HTTP app to start,
-// but feel free to replace it with Express, Koa, etc
 var app = function(req, res) {
     res.end('Hello, Encrypted World!');
 };
